4752b2638d7cbbe27f67c52c7c01d00a9a172c78
Darryl Wagoner <darryl@wagoner.me>
2014-12-25 03:57:26 -0600

    Added solution/project and QsoPublic

---
+++ LogbookService/QsoPublic/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("QsoPublic")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QsoPublic")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cbe36ad-882d-4e63-a912-eb09665e3445")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2014.12.25.1")]
+[assembly: AssemblyFileVersion("2014.12.25.1")]
---
+++ LogbookService/QsoPublic/QsoPublic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class QsoPublic
+    {
+        public long Id { get; set; }
+        public string MyCall { get; set; }
+        public string TheirCall { get; set; }
+        public string Dxcc { get; set; }
+        public string ContestId { get; set; }
+        public string Comments { get; set; }
+        public string Computer { get; set; }
+        public string Continent { get; set; }
+        public string Country { get; set; }
+        public string TheirCounty { get; set; }
+        public string MyCounty { get; set; }
+        public string CQZone { get; set; }
+        public DateTime StartDTG { get; set; }
+        public string Fist { get; set; }
+        public decimal Frequency { get; set; }
+        public string MyGrid { get; set; }
+        public string TheirGrid { get; set; }
+        public string ITUZone { get; set; }
+        public string Operator { get; set; }
+        public string IOTA { get; set; }
+        public string LightHouse { get; set; }
+        public string Mode { get; set; }
+        public string ContestMode { get; set; }
+        public string TheirName { get; set; }
+        public int Power { get; set; }
+        public string Precedence { get; set; }
+        public string PropMode { get; set; }
+        public string LoTWQsl { get; set; }
+        public string EQsl { get; set; }
+        public string RecRST { get; set; }
+        public string SentRst { get; set; }
+        public string SatName { get; set; }
+        public string Section { get; set; }
+        public string SerialNum { get; set; }
+        public string USState { get; set; }
+        public string TenTen { get; set; }
+        public string LotwSig { get; set; }
+
+    }
+}
